00e8a7480008fd0a4d936886c50753565b58590a
Francisco <fjhuertru@gmail.com>
2026-05-07 13:00:14 -0600

    IDK Backend IG

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -6,7 +6,7 @@ import ChartPanel from "./components/ChartPanel";
 const CAPACIDAD = 20;
 
 // 🔌 Cambia la IP y puerto por donde corre tu FastAPI
-const API_URL = "https://localhost:8000";
+const API_URL = "http://192.168.1.115:8000/ultimo";
 
 export default function App() {
   const [data, setData]       = useState([]);
